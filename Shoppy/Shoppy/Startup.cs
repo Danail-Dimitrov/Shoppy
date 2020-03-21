c9989a73ce50c9bb5331b2decfb10a61b550513d
Danail Dimitrov <danaildimitrov321@gmail.com>
2020-03-21 23:52:55 +0200

    Default admin mysql db andusername instead of email for log in

--- Shoppy/Shoppy/Startup.cs
+++ Shoppy/Shoppy/Startup.cs
@@ -30,14 +30,15 @@ namespace Shoppy
             services.AddDbContext<ApplicationDbContext>(options =>
              options.UseMySql(
                  Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+                .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider services)
         {
             if(env.IsDevelopment())
             {
@@ -64,7 +65,44 @@ namespace Shoppy
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
+                CreateRoles(services).Wait();
             });
+        }
+
+        private async Task CreateRoles(IServiceProvider serviceProvider)
+        {
+            var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var UserManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+            string[] roles = { "Admin", "Seller", "Buyer" };
+            IdentityResult roleResult;
+
+            foreach(var role in roles)
+            {
+                var roleCheck = await RoleManager.RoleExistsAsync(role);
+                if(!roleCheck)
+                {
+                    //here in this line we are creating a role and seed it to the database
+                    roleResult = await RoleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+
+            //here we are assigning the Admin role to the User that we have registered above 
+            //Now, we are assinging admin role to this user("Ali@gmail.com"). When will we run this project then it will
+            //be assigned to that user.
+            string adminEmail = "admin@email.com";            
+            IdentityUser user = await UserManager.FindByEmailAsync(adminEmail);
+            if(user == null)
+            {
+                string adminUserName = "Admin";
+                string adminPhoneNumber = "5558889999";
+                string adminPassword = "Password1.";
+
+                var adminUser = new IdentityUser { UserName = adminUserName, Email = adminEmail, EmailConfirmed = true, PhoneNumber = adminPhoneNumber, PhoneNumberConfirmed = true };
+                var result = await UserManager.CreateAsync(adminUser, adminPassword);
+
+                await UserManager.AddToRoleAsync(adminUser, "Admin");
+            }          
         }
     }
 }
+ 
